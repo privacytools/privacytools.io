bdea0c44b2e87bf84ea56051d75e5411b065ff03
privacytoolsIO <privacy@tuta.io>
2015-08-14 13:24:17 +0800

    piwik code removed

--- pw.php
+++ pw.php
@@ -146,22 +146,5 @@ $pws8 = SecurePasswordGenerator(8,false,'luds');
 </p>
 
 </div>
-
-<!-- Start Piwik -->
-<script type="text/javascript">
-  var _paq = _paq || [];
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="//www.privacytools.io/stats/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', 1]);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<noscript><p><img src="//www.privacytools.io/stats/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
-<!-- End Piwik Code -->
-
 </body>
 </html>
