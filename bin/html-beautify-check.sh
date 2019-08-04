271cee8bca32fba944d60081d5488fad0b34c670
nitrohorse <1514352+nitrohorse@users.noreply.github.com>
2019-08-03 20:34:35 -0700

    Update package.json

--- bin/html-beautify-check.sh
+++ bin/html-beautify-check.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# A custom script, that formats the file(s) with html-beautify,
-# but instead of changing it, it only errors with 0/1 if it needs to be changed
-# Ref: https://web.archive.org/web/20190522172323/https://medium.com/@jtomaszewski/keep-your-code-clean-forever-65c71f7f2df
+# Runs html-beautify's format check and errors with 0/1 if it needs to be changed.
 APPDIR=$(readlink -f $(dirname $(dirname $0)))
 TMPDIR=$(mktemp -d)
 cd $APPDIR
