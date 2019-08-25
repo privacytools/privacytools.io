4cd0f2e7355186bf49d5a864f091d982258852b2
Dawid Potocki <dpot@disroot.org>
2019-08-25 04:01:50 +0000

    Make JavaScript work with LibreJS (#1213)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,8 @@
-// Tooltips
-$(function () {
-   $("[data-toggle='tooltip']").tooltip();
+$(function() {
+  $("[data-toggle='tooltip']").tooltip();
 });
 
-
-//
 // Navbar dropdowns
-//
-
 const navSections = document.querySelectorAll(".nav-details");
 
 navSections.forEach(navSection => {
@@ -31,3 +26,22 @@ function navSectionsClose(event) {
     navSection.open = !open;
   });
 }
+
+// Matomo
+var _paq = window._paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(["trackPageView"]);
+_paq.push(["enableLinkTracking"]);
+(function() {
+  var u = "https://stats.privacytools.io/";
+  _paq.push(["setTrackerUrl", u + "matomo.php"]);
+  _paq.push(["setSiteId", "1"]);
+  var d = document,
+    g = d.createElement("script"),
+    s = d.getElementsByTagName("script")[0];
+  g.type = "text/javascript";
+  g.async = true;
+  g.defer = true;
+  g.src = u + "matomo.js";
+  s.parentNode.insertBefore(g, s);
+})();
