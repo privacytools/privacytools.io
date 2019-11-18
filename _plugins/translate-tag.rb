c36002d85192a38ddc4418f477996d8f9fabb3f3
djoate <56777051+djoate@users.noreply.github.com>
2019-11-18 00:09:19 +0000

    Add jekyll-simple-i18n plugin with example
    Adds in support for https://github.com/signalapp/jekyll-simple-i18n.
    
    The plugin did not work with the github-pages gem, so github-pages was
    switched with jekyll (which is what the current i18n branch does anyway).

---
+++ _plugins/translate-tag.rb
@@ -0,0 +1,38 @@
+# Copyright 2019 Signal
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+module Jekyll
+
+  class TranslateTag < Liquid::Tag
+    attr_reader :text, :weblate_id
+
+    def initialize(tag_name, text, tokens)
+      super
+      @text         = text.strip
+      @weblate_id = Weblate::ID.get(text)
+    end
+
+    def render(context)
+      page_language = context.environments.first["page"]["language"]
+      site          = context.registers[:site]
+
+      if page_language.nil?
+        Weblate::SourceFile.add_entry(weblate_id, text)
+
+        text
+      else
+        translated_string = site.data["languages"][page_language][weblate_id]
+
+        translated_string.nil? ? text : translated_string.strip
+      end
+    end
+  end
+
+end
+
+Liquid::Template.register_tag('translate', Jekyll::TranslateTag)
---
+++ _plugins/weblate-source-file.rb
@@ -0,0 +1,49 @@
+# Copyright 2019 Signal
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+module Weblate
+
+  class ID
+    def self.get(source_text)
+      source_text.nil? ? source_text : source_text.strip[0..100].gsub(/[^\w\d\s]/, '').tr(' ', '_').downcase
+    end
+  end
+
+  class SourceFile
+    class << self
+      def add_entry(weblate_id, source_text)
+        unless id_already_exists?(weblate_id) or source_text.nil?
+          new_entry = <<-YAML
+#{weblate_id}: |
+  #{source_text}
+
+          YAML
+
+          File.write(weblate_source_location, new_entry, mode: 'a')
+        end
+      end
+
+      def id_already_exists?(weblate_id)
+        File.readlines(weblate_source_location).grep(/^#{weblate_id}: \|$/).any?
+      end
+
+      def refresh
+        File.write(weblate_source_location, "---\n")
+      end
+
+      def weblate_source_location
+        "#{__dir__}/../weblate-source-file.yml"
+      end
+    end
+  end
+
+end
+
+Jekyll::Hooks.register :site, :after_init do
+  Weblate::SourceFile.refresh
+end
