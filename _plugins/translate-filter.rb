9924c83b85c376c6f2c8334fdb84cd60c151b127
djoate <56777051+djoate@users.noreply.github.com>
2019-11-23 16:33:27 -0500

    Don't add empty strings into source file

--- _plugins/translate-filter.rb
+++ _plugins/translate-filter.rb
@@ -1,18 +1,23 @@
 module Jekyll
   module TranslateFilter
     def translate_text(input)
-      text          = input.strip
-      weblate_id    = Weblate::ID.get(input)
-      page_language = @context.environments.first["page"]["language"]
-      site          = @context.registers[:site]
-      if page_language.nil?
-        Weblate::SourceFile.add_entry(weblate_id, text)
-
-        text
+      if input.nil? or input.empty?
+        input
       else
-        translated_string = site.data["languages"][page_language][weblate_id]
+        text          = input.strip
+        weblate_id    = Weblate::ID.get(input)
+        page_language = @context.environments.first["page"]["language"]
+        site          = @context.registers[:site]
+        
+        if page_language.nil?
+          Weblate::SourceFile.add_entry(weblate_id, text)
 
-        translated_string.nil? ? text : translated_string.strip
+          text
+        else
+          translated_string = site.data["languages"][page_language][weblate_id]
+
+          translated_string.nil? ? text : translated_string.strip
+        end
       end
     end
 
